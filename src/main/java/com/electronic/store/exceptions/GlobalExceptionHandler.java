6c590db1252224d16f854591249c8d1b517b6361
Ankush sachan <ankushsachan327@gmail.com>
2024-12-01 20:25:49 +0530

    Changes in GlobalExceptionHandler

--- src/main/java/com/electronic/store/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/electronic/store/exceptions/GlobalExceptionHandler.java
@@ -6,8 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -23,4 +30,25 @@ public class GlobalExceptionHandler {
        ApiResponseMessage responseMessage= ApiResponseMessage.builder().message(ex.getMessage()).status(HttpStatus.NOT_FOUND).success(true).build();
         return new ResponseEntity<>(responseMessage,HttpStatus.NOT_FOUND);
     }
+
+
+    // MethodArgumentNotValidException
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,Object>> handlerMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+
+
+        List<ObjectError> allError=ex.getBindingResult().getAllErrors();
+        Map<String,Object> response= new HashMap<>();
+        allError.stream().forEach(objectError -> {
+            String message = objectError.getDefaultMessage();
+            String field = ((FieldError) objectError).getField();
+            response.put(field,message);
+        });
+
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+    }
+
+
+
 }
